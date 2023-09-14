3946de07ec4f18df3f222dd6b05e31c8637bbc7e
ksvengurlekar-tamu <142921165+ksvengurlekar-tamu@users.noreply.github.com>
2023-09-14 14:19:25 -0500

    Added button + JS file + style sheet 2

---
+++ toggle_style.js
@@ -0,0 +1,27 @@
+function toggleStyleSheet(){
+    // Get style element by ID 
+    el = document.getElementById("mainStyleSheet");
+
+    //Check the current stylesheet file name.
+    if (el.getAttribute("href") === "darker_styles.css") {
+        el.setAttribute("href", "lighter_styles.css");
+        localStorage.setItem("href", "lighter_styles.css");
+    }
+    else {
+        el.setAttribute("href", "darker_styles.css");
+        localStorage.setItem("href", "darker_styles.css");
+    }
+
+}
+
+
+window.onload = function(){
+    // Makes the last div color persist even when someone refreshes the page.
+
+    // get stylesheet name from local storage
+    style = localStorage.getItem("href")
+
+    // get html style element by ID an replace href attribute of html element.
+    document.getElementById("mainStyleSheet").setAttribute("href", style);
+
+}
